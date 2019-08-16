1a8ba48febd4b6bfb0f892dacda233a121869c72
Praktikant | pc-web <praktikant@pc-web.at>
2019-08-16 09:35:31 +0200

    prepare command added

---
+++ src/commands/prepare.js
@@ -0,0 +1,32 @@
+const {Command, flags} = require('@oclif/command');
+
+const fs = require('fs');
+const path = require('path');
+var child_process = require('child_process');
+
+class prepare extends Command {
+    async run() {
+        let oclif = this.log;
+        const {flags} = this.parse(prepare)
+        child_process.exec('install.bat', function(error, stdout, stderr) {
+          if(stderr){
+              oclif(stderr);
+          }
+          else if(stdout){
+              oclif(stdout);
+          }
+          else if(error){
+              oclif(error);
+          }
+        });
+    }
+      
+}
+
+prepare.description = `prepares the server`
+
+prepare.flags = {
+  name: flags.string({char: 'n', description: 'name to print'}),
+}
+
+module.exports = prepare
