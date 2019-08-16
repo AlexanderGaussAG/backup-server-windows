1c21576a4dace4402b80e6b968e063d6b8a4de78
Praktikant | pc-web <praktikant@pc-web.at>
2019-08-16 09:19:08 +0200

    fixed install.bat

--- install.bat
+++ install.bat
@@ -4,5 +4,6 @@ mkdir logs
 mkdir configs
 mkdir MSSQL-Backup-Config
 cd MSSQL-Bakup-Config
+cd MSSQL-Backup-Config
 echo {"smtp": {"host": "<your smtp server>","secure": true, "port": 465, "auth": {"user": "<username>", "password": "<password>"}}} > mail.json
-code mail.json
+notepad.exe mail.json
