7be21b48dfa9c96dd589f2a97c964acf7b7b60e1
Praktikant | pc-web <praktikant@pc-web.at>
2019-08-19 11:07:30 +0200

    removed all console.logs

--- src/lib/ttl.js
+++ src/lib/ttl.js
@@ -4,7 +4,6 @@ const log = require("./logger");
 
 function init(param){
     for(i=0; i<= (param.backupFolder).length-1; i++){
-        console.log(param.backupFolder[i].path)
         getAllConfigFiles(param.backupFolder[i].path, (res) => {
             if(res.length > param.ttl){
                 fs.unlink(res[0], (err) => {
@@ -21,7 +20,6 @@ function init(param){
 }
 
 function getAllConfigFiles(param, callback){
-    console.log(param)
     let ConfDir = param; //get directory which contains the files
     fs.readdir(ConfDir, (err, files) => {
         if(err){
