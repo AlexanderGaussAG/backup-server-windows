88f560de53971f3a45d2f4a983fbd7fbe71d6678
Praktikant | pc-web <praktikant@pc-web.at>
2019-08-19 11:48:29 +0200

    fixed mail

--- src/lib/backup.js
+++ src/lib/backup.js
@@ -3,7 +3,6 @@ const path = require('path');
 const fs = require('fs');
 const { base64encode, base64decode } = require('nodejs-base64');
 const mail = require("./mail");
-const param = require("C:\\MSSQL-Backup\\MSSQL-Backup-Config\\mail.json");
 
 function init(params, callback){
     for(let i = 0; i<= (params.backupFolder).length-1;i++){
@@ -12,19 +11,19 @@ function init(params, callback){
         datetime = (""+datetime).split(":").join("-");
         const backup = `SqlCmd -S "${(params.db.server).split("/").join("\\")}" -U ${params.db.user} -P ${base64decode(params.db.password)} -Q "BACKUP DATABASE [${params.db.database}] TO DISK='${(params.backupFolder[i].path).split("/").join("\\")+params.db.database+datetime+".bak"}'"`
         exec(backup, (err, stdout, stderr) => {
-            if(err){callback(err); mail(param, false)}
+            if(err){callback(err); mail(params, false, i)}
             else if(stdout){
                 const validate = `SqlCmd -S "${(params.db.server).split("/").join("\\")}" -U ${params.db.user} -P ${base64decode(params.db.password)} -Q "RESTORE VERIFYONLY FROM DISK = '${(params.backupFolder[i].path).split("/").join("\\")+params.db.database+datetime+".bak"}'"`
                 exec(validate, (err, stdout, stderr) => {
-                    if(err){callback(err); mail(param, false)}
+                    if(err){callback(err); mail(params, false, i)}
                     else if(stdout){
                         callback(stdout);
-                        mail(param, true);
+                        mail(params, true, i);
                     }
-                    else if(stderr){callback(stderr); mail(param, false)}
+                    else if(stderr){callback(stderr); mail(params, false, i)}
                 })
             }
-            else if(stderr){callback(stderr); mail(param, false)}
+            else if(stderr){callback(stderr); mail(params, false, i)}
         });
     }
 }
--- src/lib/mail.js
+++ src/lib/mail.js
@@ -1,15 +1,17 @@
 const nodemailer = require('nodemailer');
 const smtpAuth = require('C:\\MSSQL-Backup\\MSSQL-Backup-Config\\mail.json');
 
-async function main(param, status){
+async function main(param, status, row){
+    console.log(param);
+    console.log(status);
 
     let transporter = nodemailer.createTransport({
-        host: smtpAuth.host,
-        port: smtpAuth.port,
-        secure: smtpAuth.secure,
+        host: smtpAuth.smtp.host,
+        port: smtpAuth.smtp.port,
+        secure: smtpAuth.smtp.secure,
         auth: {
-            user: smtpAuth.auth.user,
-            pass: smtpAuth.auth.password
+            user: smtpAuth.smtp.auth.user,
+            pass: smtpAuth.smtp.auth.password
         }
     });
 
@@ -18,20 +20,20 @@ async function main(param, status){
     if(status == true){
       transporter.sendMail({
             from: '"MSSQL-Backup" <mssqlbackup@pc-web.at>',
-            to: to,
+            to: param.email,
             subject: "Backup Erfolgreich",
-            text: `Das Backup wurde erfolgreich erstellt und auf dem Pfad ${param.backupFolder[0].path}  hinterlegt`,
-            html: `<b><h1>BACKUP ERFOLGREICH</h1>\nDas Backup von ${param.db.database} wurde erfolgreich erstellt und auf dem Pfad "${param.backupFolder[0].path}" hinterlegt <hr> ${crDate}</b>`
+            text: `Das Backup wurde erfolgreich erstellt und auf dem Pfad ${param.backupFolder[row].path}  hinterlegt`,
+            html: `<b><h1>BACKUP ERFOLGREICH</h1>\nDas Backup von ${param.db.database} wurde erfolgreich erstellt und auf dem Pfad "${param.backupFolder[row].path}" hinterlegt <hr> ${crDate}</b>`
         });
     }
 
     else{
      transporter.sendMail({
             from: '"MSSQL-Backup" <mssqlbackup@pc-web.at>',
-            to: to,
+            to: param.email,
             subject: "Backup Nicht Erfolgreich",
-            text: `Das Backup wurde erfolglos erstellt und nicht auf dem ${param.backupFolder[0].path} hinterlegt`,
-            html: `<b><h1>BACKUP NICHT ERFOLGREICH</h1>\nDas Backup von ${param.db.database} wurde erfolglos erstellt und nicht auf dem Pfad "${param.backupFolder[0].path}" hinterlegt <hr> ${crDate}</b>`
+            text: `Das Backup wurde erfolglos erstellt und nicht auf dem ${param.backupFolder[row].path} hinterlegt`,
+            html: `<b><h1>BACKUP NICHT ERFOLGREICH</h1>\nDas Backup von ${param.db.database} wurde erfolglos erstellt und nicht auf dem Pfad "${param.backupFolder[row].path}" hinterlegt <hr> ${crDate}</b>`
         });
     }
 
